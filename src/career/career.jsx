1c08f7e15fc38aab9a2f278734cc35f13687cd65
RowanKinross <rowankinross@yahoo.co.uk>
2026-09-24 17:34:51 +0100

    career related content

--- src/career/career.jsx
+++ src/career/career.jsx
@@ -1,7 +1,14 @@
+import './career.css'
+
 function Career() {
   return (
     <section>
       <h2>Career</h2>
+      <p className="career-question"> What was something career-related that you worked on this week?</p>
+      <form>
+         <textarea name="careerGoal" className="career-input" rows="5" />
+      </form>
+      <button type="submit">Save</button>
     </section>
   )
 }
